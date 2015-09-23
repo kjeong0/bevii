74af42011d031f56851b088a896364a6a1afaa58
James Cagalawan <ajcagala@uwaterloo.ca>
2015-09-23 15:34:14 -0400

    1v1 RPS now working

--- sefydp.js
+++ sefydp.js
@@ -27,25 +27,53 @@ if (Meteor.isClient) {
                     {playerTwo: Meteor.user().username}
                 ]
             });
+        },
+        waitingForOtherPlayer: function() {
+            /* Returns true if opponent still has to make a move */
+            var otherPlayer = Meteor.user().username === this.playerOne ? this.playerTwo : this.playerOne;
+            var match = Matches.find(this._id);
+            return this[otherPlayer] === undefined;
+        },
+        makeMove: function() {
+            /* Return true if the player still has to make a move */
+            return this[Meteor.user().username] === undefined;
         }
     });
 
     Template.rps.events({
-        'click .rps': event => {
-            var thisPlayer = Meteor.userId();
-            var filter = {_id: this._id};
-            console.log(Matches.findOne(filter));
-            var match = Matches.findOne({_id: this._id});
-            if (match.playerOne === thisPlayer) {
-                match.playerOneChoice = event.target.value;
+        'click .rps': function(event) {
+            // Add a key indicating the user's choice and checks for winner if possible
+            var updateQuery = {$set: {}};
+            var choice = event.target.value;
+            updateQuery.$set[Meteor.user().username + ".choice"] = event.target.value;
+            Matches.update(this._id, updateQuery);
+
+            var otherPlayer = Meteor.user().username === this.playerOne ? this.playerTwo : this.playerOne;
+            if(this[otherPlayer] !== undefined) {
+                /* Really ugly way to find the winner, refactor this later */
+                var getWinner = function(playerOneName, playerTwoName) {
+                    var playerOneChoice = choice;
+                    var playerTwoChoice = this[playerTwoName].choice;
+                    if ((playerOneChoice === "rock" && playerTwoChoice ==="scissors") ||
+                        (playerOneChoice === "scissors" && playerTwoChoice ==="paper") ||
+                        (playerOneChoice === "paper" && playerTwoChoice ==="rock")) {
+                        Matches.update(this._id, {$set: {winner: playerOneName}});
+                    } else if ((playerTwoChoice === "rock" && playerOneChoice ==="scissors") ||
+                               (playerTwoChoice === "scissors" && playerOneChoice ==="paper") ||
+                               (playerTwoChoice === "paper" && playerOneChoice ==="rock")) {
+                        Matches.update(this._id, {$set: {winner: playerTwoName}});
+                    } else {
+                        Matches.update(this._id, {$set: {winner: "tie"}});
+                    }
+                }.bind(this);
+                getWinner(Meteor.user().username, otherPlayer);
             }
-            Matches.update(this._id, match);
         }
     });
 
     Template.userCount.helpers({
         usersOnline: () => {
-            return Meteor.users.find({"status.online": true}, {$neq: {"_id": Meteor.userId()}});
+            return Meteor.users.find({"status.online": true, _id: {$ne: Meteor.userId()}});
         },
         usersOnlineCount: () => {
             return Meteor.users.find({ "status.online": true}).count();
@@ -53,7 +81,7 @@ if (Meteor.isClient) {
     });
 
     Template.userCount.events({
-        "click .user": event => {
+        "click .user": function(event) {
             console.log(event);
             console.log(this);
 
